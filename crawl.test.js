a8ae84b453fe054d13bb4af9987b6844a08b21ac
Pranish Chaulagain <cpranishabc163@gmial0.com>
2025-02-25 13:45:16 +0545

    Added getURLsFromHTML function with tests

--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const { normalizeURL } = require("./crawl");
+const { normalizeURL, getURLsFromHTML } = require("./crawl");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL function: Strip Protocol", () => {
@@ -26,5 +26,37 @@ test("normalizeURL function: Strip Http", () => {
   const input = "http://Google.com/path/";
   const actual = normalizeURL(input);
   const expected = "google.com/path";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML function: Absolute URLs", () => {
+  const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="https://google.com/path/">
+                Google
+            </a>
+        </body>
+    </html>
+    `;
+  const inputBaseURL = "https://google.com";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://google.com/path/"];
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML function: Relative URLs", () => {
+  const inputHTMLBody = `
+    <html>
+        <body>
+            <a href="/path/">
+                Google
+            </a>
+        </body>
+    </html>
+    `;
+  const inputBaseURL = "https://google.com";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = ["https://google.com/path/"];
   expect(actual).toEqual(expected);
 });
