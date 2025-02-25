176c09e635923a54d7776935b8f99d35905e830c
Pranish Chaulagain <cpranishabc163@gmial0.com>
2025-02-25 13:56:44 +0545

    Added crawlPage function with tests

--- crawl.js
+++ crawl.js
@@ -37,4 +37,55 @@ function getURLsFromHTML(htmlBody, baseURL) {
   return urls;
 }
 
+async function crawlPage(baseURL, currentURL, pages) {
+  const baseURLObj = new URL(baseURL);
+  const currentURLObj = new URL(currentURL);
+  if (baseURLObj.hostname !== currentURLObj.hostname) {
+    return pages;
+  }
+
+  const normalizeCurrentURL = normalizeURL(currentURL);
+  if (pages[normalizeCurrentURL] > 0) {
+    pages[normalizeCurrentURL]++;
+    return pages;
+  }
+
+  pages[normalizeCurrentURL] = 1;
+
+  console.log(`Actively crawling: ${currentURL}`);
+
+  try {
+    const response = await fetch(currentURL);
+
+    if (response.status > 399) {
+      console.log(
+        `Error in fetch with status code: ${response.status}, on page: ${currentURL}`
+      );
+
+      return pages;
+    }
+
+    const contentType = response.headers.get("content-type");
+    if (!contentType.includes("text/html")) {
+      console.log(
+        `Non html response, content type: ${contentType}, on page: ${currentURL}`
+      );
+
+      return pages;
+    }
+
+    const htmlBody = await response.text();
+
+    const nextURLs = getURLsFromHTML(htmlBody, baseURL);
+
+    for (const nextURL of nextURLs) {
+      pages = await crawlPage(baseURL, nextURL, pages);
+    }
+  } catch (err) {
+    console.log(`Error in fetch: ${err.message}, on page: ${currentURL}`);
+  }
+
+  return pages;
+}
+
 module.exports = { normalizeURL, getURLsFromHTML };
